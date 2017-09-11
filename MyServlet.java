6ec2a8bf2ddda1ee5513ba0723ff597e7b6ba3c7
jlrifer <31681347+jlrifer@users.noreply.github.com>
2017-09-10 23:32:52 -0500

    Add files via upload
    Added html
    Updates to servlet

--- MyServlet.java
+++ MyServlet.java
@@ -7,6 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +19,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/MyServlet")
 public class MyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	static String             url              = "jdbc:mysql://ec2jlrifer.ddns.net:3306/myDB";
+	static String             user             = "newremoteuser";
+	static String             password         = "password";
+	static Connection         connection       = null;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -31,8 +36,52 @@ public class MyServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Testing");
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().println("<head><title>Assignment List</title></head>");
+		response.getWriter().println("-------- MySQL JDBC Connection Testing ------------<br>");
+		  
+		try {
+		    Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+		    System.out.println("Where is your MySQL JDBC Driver?");
+		    e.printStackTrace();
+		    return;
+		}
+		connection = null;
+		  
+		try {
+		   connection = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+		   System.out.println("Connection Failed! Check output console");
+		   e.printStackTrace();
+		   return;
+		}
+		if (connection == null) {
+		   System.out.println("Failed to make connection!");
+		}
+		
+		try {
+			String selectSQL = "SELECT * FROM assignment ";
+			String theUserName = "user%";
+	   		response.getWriter().println(selectSQL + "<br>");
+			  response.getWriter().println("------------------------------------------<br>");
+			  PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
+			  preparedStatement.setString(1, theUserName);
+			  ResultSet rs = preparedStatement.executeQuery();
+		 
+			while (rs.next()) {
+			  String id = rs.getString("ID");
+			  String username = rs.getString("MYUSER");
+			  String email = rs.getString("EMAIL");
+			  String phone = rs.getString("PHONE");
+			  response.getWriter().append("USER ID: " + id + ", ");
+			  response.getWriter().append("USER NAME: " + username + ", ");
+			  response.getWriter().append("USER EMAIL: " + email + ", ");
+			  response.getWriter().append("USER PHONE: " + phone + "<br>");
+		   }
+		} catch (SQLException e) {
+		     e.printStackTrace();
+		}
 	}
 
 	/**
